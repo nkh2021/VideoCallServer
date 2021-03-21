2094b9bf249c56cbcab9f3c43018e79b0e40e2b0
nkh2021 <nkhademi2018@gmail.com>
2021-03-21 09:23:02 +0330

    removing app.get from  Server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 // app.listen(3000, () => {
 //   console.log("Server is running on port 3000");
 // });
-app.get("/", (req, res) => res.send("Server is runing"));
+
 let connectedUsers = [];
 
 //Socket on conneciton
